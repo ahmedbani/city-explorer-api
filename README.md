8b55f43c78a598365bc1baa6bf12234ff42d88f1
ahmedbani <a13banisal@gmail.com>
2021-09-06 21:40:11 +0300

    lab7 backend

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # city-explorer-api
+
+**Author**: Ahmed Bani-Salameh
+**Version**: 1.0.1
+
+## Overview
+
+this is an API server created locally to get the request that is sent from the city-explorer app and send the response with the data 
+
+## Getting Started
+
+## Architecture
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+languages : JavaScript  
+framework : express  
+
+## Change Log
+<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
+
+01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource. -->
+
+## Credit and Collaborations
+<!-- Give credit (and a link) to other people or resources that helped you build this application. -->
---
+++ server.js
@@ -0,0 +1,45 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const weatherData = require('./data/weather.json');
+const cors = require('cors');
+
+const server = express();
+server.use(cors());
+
+const PORT = process.env.PORT;
+
+class Forecast {
+    constructor(date,description){
+        this.date = date;
+        this.description = description;
+    }
+    
+}
+//http://localhost:4000/weather?lat=47.6038321&lon=-122.3300624/
+server.get('/weather',(req,res) => {
+    const latitude = req.query.lat;
+    const longitude = req.query.lon;
+    const result = weatherData.find(item => {
+        if(item.lat === latitude && item.lon === longitude)
+        return item.data;
+        else
+        return 'ERROR, no city data';
+    })
+    let objArr = result.data.map(item => {
+        let newObject = new Forecast(item.datetime,item.weather.description)
+        return newObject;
+    });
+    res.send(objArr);
+})
+
+//http://localhost:4000/**** */
+server.get('*',(req,res) => {
+    res.status(404).send('sorry, page not found');
+})
+
+server.listen(PORT,() => {
+    console.log(`Hi, listening from PORT: ${PORT}`)
+})
+
