6f519fa62e4686d9188a0195048025dde761ecbe
ahmedbani <a13banisal@gmail.com>
2021-09-07 23:11:02 +0300

    lab8

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
-const weatherData = require('./data/weather.json');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
 const server = express();
 server.use(cors());
@@ -11,40 +11,58 @@ server.use(cors());
 const PORT = process.env.PORT;
 
 class Forecast {
-    constructor(item){
-        this.date = item.valid_date;
-        this.description = `low of ${item.low_temp}, high of ${item.high_temp} with ${item.weather.description}`;
-    }
-    
+  constructor(item) {
+    this.date = item.valid_date;
+    this.description = `low of ${item.low_temp}, high of ${item.high_temp} with ${item.weather.description}`;
+  }
 }
 //http://localhost:4000/weather?lat=47.6038321&lon=-122.3300624/
-server.get('/weather',(req,res) => {
-    const latitude = req.query.lat;
-    const longitude = req.query.lon;
-    const result = weatherData.find(item => {
-        if(item.lat == latitude && item.lon == longitude){
-            return item;
-        }else{
-            console.log('err');
-        }
-    
-    });
-    console.log(result);
-    let objArr = result.data.map(item => {
-        // console.log(item);
-        let newObject = new Forecast(item)
-        
-        return newObject;
-    });
-    res.send(objArr);
-})
+server.get("/weather", getWeatherData);
+
+function getWeatherData(req, res) {
+  const latitude = req.query.lat;
+  const longitude = req.query.lon;
+  const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${latitude}&lon=${longitude}&key=${process.env.WEATHER_KEY}`;
+
+  axios
+    .get(url)
+    .then((result) => {
+      let weatherData = result.data.data.map((item) => new Forecast(item));
+      res.send(weatherData);
+    })
+    .catch((err) => res.status(404).send("sorry, page not found"));
+}
+//http://localhost:4000/movies?api_key=
+server.get('/movies',getMoviesData);
+
+function getMoviesData(req,res){
+    const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.MOVIE_KEY}`
+
+    axios
+    .get(url)
+    .then(result => {
+        console.log(result);
+        let movies = result.data.results.map(item => new Movie(item));
+        res.send(movies);
+    })
+    .catch((err) => res.status(404).send("sorry, page not found"));
+}
 
 //http://localhost:4000/**** */
-server.get('*',(req,res) => {
-    res.status(404).send('sorry, page not found');
-})
+server.get("*", (req, res) => {
+  res.status(404).send("sorry, page not found");
+});
 
-server.listen(PORT,() => {
-    console.log(`Hi, listening from PORT: ${PORT}`)
-})
+function Movie (item){
+    this.title= item.title ;
+    this.overview= item.overview ;
+    this.average_votes= item.vote_average;
+    this.total_votes= item.vote_count;
+    this.image_url= item.poster_path;
+    this.popularity= item.popularity;
+    this.released_on= item.release_date;
+}
 
+server.listen(PORT, () => {
+  console.log(`Hi, listening from PORT: ${PORT}`);
+});
