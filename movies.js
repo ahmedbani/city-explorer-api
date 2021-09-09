4383bfad46b8b74b6c7267736a32074cb2ac994c
ahmedbani <a13banisal@gmail.com>
2021-09-09 13:15:13 +0300

    data persistent

--- movies.js
+++ movies.js
@@ -14,18 +14,24 @@ class Movie {
       this.released_on= item.release_date;
   }
 }
-
+let memory ={};
 //https://api.themoviedb.org/3/search/movie?query=cityname&key
 obj.getMoviesData = function (req,res){
     const city = req.query.query;
-    const url = `https://api.themoviedb.org/3/search/movie?query=${city}&api_key=${process.env.MOVIE_KEY}`
+
+    if(memory[city !== undefined]){
+        res.send(memory[city]);
+    }else{
+    const url = `https://api.themoviedb.org/3/search/movie?query=${city}&api_key=${process.env.MOVIE_KEY}`;
 
     axios
     .get(url)
     .then(result => {
         let movies = result.data.results.map(item => new Movie(item));
+        memory[city] = movies;
         res.send(movies);
     })
     .catch((err) => res.status(404).send("sorry, page not found"));
+}
 }
 module.exports = obj ;
